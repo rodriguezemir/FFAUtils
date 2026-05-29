91f99dca8cb42359e6a707ec45d55b4e7301ba0c
volcqnn <emir49499141@gmail.com>
2026-05-29 14:32:08 -0300

    init commit

---
+++ src/main/java/site/zvolcan/fFAUtils/FFAUtils.java
@@ -0,0 +1,14 @@
+package site.zvolcan.fFAUtils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FFAUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
