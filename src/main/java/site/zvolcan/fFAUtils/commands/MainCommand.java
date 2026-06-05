d6796428e0e61ef6801e35ede0ceac86060ca1d0
rodriguezemir <emir49499141@gmail.com>
2026-06-05 20:40:18 -0300

    feat: add reload command to MainCommand for plugin reloading

--- src/main/java/site/zvolcan/fFAUtils/commands/MainCommand.java
+++ src/main/java/site/zvolcan/fFAUtils/commands/MainCommand.java
@@ -4,14 +4,34 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import me.putindeer.api.util.PluginUtils;
 import org.bukkit.command.CommandSender;
 import site.zvolcan.fFAUtils.commands.abs.CommandExecutor;
+import site.zvolcan.fFAUtils.objects.Sounds;
 
 public final class MainCommand implements CommandExecutor {
 
+    private final PluginUtils utils;
+
+    public MainCommand(PluginUtils utils) {
+        this.utils = utils;
+    }
+
     @Override
-    public LiteralCommandNode<CommandSourceStack> execute(CommandSender sender, String[] args) {
+    public LiteralCommandNode<CommandSourceStack> execute() {
         LiteralArgumentBuilder<CommandSourceStack> literal = Commands.literal("ffautils");
+
+        literal.then(Commands.literal("reload").executes((ctx) -> {
+            CommandSourceStack source = ctx.getSource();
+            CommandSender sender = source.getSender();
+
+            utils.message(
+                    sender,
+                    Sounds.SUCCESS_SOUND,
+                    "<green>FFAUtils has been reloaded."
+            );
+            return 0;
+        }));
 
         return literal.build();
     }
