dc8669831f5fa4afc75d8b3b8c667587e12740d2
rodriguezemir <emir49499141@gmail.com>
2026-06-05 20:40:07 -0300

    feat: add KitCommand for managing player kits

---
+++ src/main/java/site/zvolcan/fFAUtils/commands/KitCommand.java
@@ -0,0 +1,122 @@
+package site.zvolcan.fFAUtils.commands;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import site.zvolcan.fFAUtils.FFAUtils;
+import site.zvolcan.fFAUtils.commands.abs.CommandExecutor;
+import site.zvolcan.fFAUtils.managers.KitManager;
+import site.zvolcan.fFAUtils.objects.Kit;
+import site.zvolcan.fFAUtils.objects.Sounds;
+
+public final class KitCommand implements CommandExecutor {
+
+    private final FFAUtils plugin;
+    private final KitManager kitManager;
+
+    public KitCommand(FFAUtils plugin, KitManager kitManager) {
+        this.plugin = plugin;
+        this.kitManager = kitManager;
+    }
+
+    @Override
+    public LiteralCommandNode<CommandSourceStack> execute() {
+        LiteralArgumentBuilder<CommandSourceStack> literal = Commands.literal("kit");
+
+        literal.then(Commands.argument("name", StringArgumentType.word()).executes(ctx -> {
+            CommandSourceStack source = ctx.getSource();
+            CommandSender sender = source.getSender();
+            if (!(sender instanceof Player player)) {
+                sender.sendMessage("Only players can execute this command.");
+                return 1;
+            }
+            String name = StringArgumentType.getString(ctx, "name");
+            Kit kit = kitManager.getKit(name);
+            if (kit == null) {
+                plugin.getUtils().message(player, Sounds.SUCCESS_SOUND, "<red>Kit '" + name + "' not found.");
+                return 1;
+            }
+            player.getInventory().setContents(kit.getContents());
+            plugin.getUtils().message(player, Sounds.SUCCESS_SOUND, "<green>Kit '" + name + "' has been applied.");
+            return 1;
+        }));
+
+        literal.then(Commands.literal("create")
+            .then(Commands.argument("name", StringArgumentType.word()).executes(ctx -> {
+                CommandSourceStack source = ctx.getSource();
+                CommandSender sender = source.getSender();
+                if (!(sender instanceof Player player)) {
+                    sender.sendMessage("Only players can execute this command.");
+                    return 1;
+                }
+                String name = StringArgumentType.getString(ctx, "name");
+                if (kitManager.getKit(name) != null) {
+                    plugin.getUtils().message(player, Sounds.SUCCESS_SOUND, "<red>Kit '" + name + "' already exists.");
+                    return 1;
+                }
+                ItemStack[] contents = player.getInventory().getContents();
+                Kit kit = new Kit(name, contents);
+                kitManager.saveKit(name, kit);
+                plugin.getUtils().message(player, Sounds.SUCCESS_SOUND, "<green>Kit '" + name + "' has been created.");
+                return 1;
+            }))
+        );
+
+        literal.then(Commands.literal("edit")
+            .then(Commands.argument("name", StringArgumentType.word()).executes(ctx -> {
+                CommandSourceStack source = ctx.getSource();
+                CommandSender sender = source.getSender();
+                if (!(sender instanceof Player player)) {
+                    sender.sendMessage("Only players can execute this command.");
+                    return 1;
+                }
+                String name = StringArgumentType.getString(ctx, "name");
+                if (kitManager.getKit(name) == null) {
+                    plugin.getUtils().message(player, Sounds.SUCCESS_SOUND, "<red>Kit '" + name + "' not found.");
+                    return 1;
+                }
+                ItemStack[] contents = player.getInventory().getContents();
+                Kit kit = new Kit(name, contents);
+                kitManager.saveKit(name, kit);
+                plugin.getUtils().message(player, Sounds.SUCCESS_SOUND, "<green>Kit '" + name + "' has been edited.");
+                return 1;
+            }))
+        );
+
+        literal.then(Commands.literal("delete")
+            .then(Commands.argument("name", StringArgumentType.word()).executes(ctx -> {
+                CommandSourceStack source = ctx.getSource();
+                CommandSender sender = source.getSender();
+                String name = StringArgumentType.getString(ctx, "name");
+                boolean deleted = kitManager.deleteKit(name);
+                if (!deleted) {
+                    sender.sendMessage(Component.text("Kit '" + name + "' not found.").color(NamedTextColor.RED));
+                    return 1;
+                }
+                sender.sendMessage(Component.text("Kit '" + name + "' has been deleted.").color(NamedTextColor.GREEN));
+                return 1;
+            }))
+        );
+
+        literal.then(Commands.literal("list").executes(ctx -> {
+            CommandSourceStack source = ctx.getSource();
+            CommandSender sender = source.getSender();
+            var kits = kitManager.getAllKits();
+            if (kits.isEmpty()) {
+                sender.sendMessage(Component.text("No kits available.").color(NamedTextColor.RED));
+                return 1;
+            }
+            sender.sendMessage(Component.text("Kits: " + String.join(", ", kits.keySet())).color(NamedTextColor.GREEN));
+            return 1;
+        }));
+
+        return literal.build();
+    }
+}
